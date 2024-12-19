b45843c8068e78768722df7b2f6647a59cea8817
JugglypuffM <stas.klinin@gmail.com>
2024-12-19 17:48:05 +0500

    feat: добавлена аутентификация при запросе данных

--- src/main/kotlin/data/DataServiceImpl.kt
+++ src/main/kotlin/data/DataServiceImpl.kt
@@ -1,6 +1,8 @@
 package data
 
+import auth.Authenticator
 import database.manager.DatabaseManager
+import domain.ResultCode
 import domain.UserInfo
 import grpc.DataProto
 import grpc.DataProto.UserDataRequest
@@ -8,7 +10,8 @@ import grpc.DataProto.UserDataResponse
 import grpc.DataServiceGrpc
 import io.grpc.stub.StreamObserver
 
-class DataServiceImpl(private val databaseManager: DatabaseManager) : DataServiceGrpc.DataServiceImplBase() {
+class DataServiceImpl(private val authenticator: Authenticator, private val databaseManager: DatabaseManager) :
+    DataServiceGrpc.DataServiceImplBase() {
     private fun createBasicDataResponse(success: Boolean, info: UserInfo): UserDataResponse {
         val data = DataProto.UserData.newBuilder()
             .setName(info.name)
@@ -26,9 +29,17 @@ class DataServiceImpl(private val databaseManager: DatabaseManager) : DataServic
         request: UserDataRequest,
         responseObserver: StreamObserver<UserDataResponse>
     ) {
-        databaseManager.getUserInformation(request.login).map<Unit> {
-            responseObserver.onNext(createBasicDataResponse(true, it))
-        }.orElse(responseObserver.onNext(createBasicDataResponse(false, UserInfo("", 0, 0, 0))))
-        responseObserver.onCompleted()
+        val result = authenticator.login(request.login, request.password)
+        when (result.resultCode) {
+            ResultCode.OPERATION_SUCCESS -> {
+                databaseManager.getUserInformation(request.login).map<Unit> {
+                    responseObserver.onNext(createBasicDataResponse(true, it))
+                }.orElse(responseObserver.onNext(createBasicDataResponse(false, UserInfo("", 0, 0, 0))))
+
+                responseObserver.onCompleted()
+            }
+
+            else -> responseObserver.onNext(createBasicDataResponse(false, UserInfo("", 0, 0, 0)))
+        }
     }
 }
