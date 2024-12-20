9fdad889261808f8a91d502524aa506852c8e2f0
JugglypuffM <stas.klinin@gmail.com>
2024-12-20 19:54:45 +0500

    feat: добавлен метод обновления информации о пользователе

--- src/main/kotlin/data/DataServiceImpl.kt
+++ src/main/kotlin/data/DataServiceImpl.kt
@@ -1,6 +1,7 @@
 package data
 
 import auth.Authenticator
+import com.google.protobuf.Empty
 import database.manager.DatabaseManager
 import domain.ResultCode
 import domain.UserInfo
@@ -8,20 +9,15 @@ import grpc.DataProto
 import grpc.DataProto.UserDataRequest
 import grpc.DataProto.UserDataResponse
 import grpc.DataServiceGrpc
+import io.grpc.Status
 import io.grpc.stub.StreamObserver
 
 class DataServiceImpl(private val authenticator: Authenticator, private val databaseManager: DatabaseManager) :
     DataServiceGrpc.DataServiceImplBase() {
     private fun createBasicDataResponse(success: Boolean, info: UserInfo): UserDataResponse {
-        val data = DataProto.UserData.newBuilder()
-            .setName(info.name)
-            .setAge(info.age)
-            .setWeight(info.weight)
-            .setTotalDistance(info.distance)
-            .build()
         return UserDataResponse.newBuilder()
             .setSuccess(success)
-            .setData(data)
+            .setData(info.toUserData())
             .build()
     }
 
@@ -35,11 +31,27 @@ class DataServiceImpl(private val authenticator: Authenticator, private val data
                 databaseManager.getUserInformation(request.login).map<Unit> {
                     responseObserver.onNext(createBasicDataResponse(true, it))
                 }.orElse(responseObserver.onNext(createBasicDataResponse(false, UserInfo("", 0, 0, 0))))
-
-                responseObserver.onCompleted()
             }
 
             else -> responseObserver.onNext(createBasicDataResponse(false, UserInfo("", 0, 0, 0)))
+        }
+        responseObserver.onCompleted()
+    }
+
+    override fun updateUserData(request: DataProto.UpdateDataRequest, responseObserver: StreamObserver<Empty>) {
+        val result = authenticator.login(request.login, request.password)
+        if (!request.hasData()) {
+            responseObserver.onError(Status.INVALID_ARGUMENT.withDescription("No data provided").asRuntimeException())
+            return
+        }
+        when (result.resultCode) {
+            ResultCode.OPERATION_SUCCESS -> {
+                databaseManager.updateUserInformation(request.login, UserInfo(request.data))
+                responseObserver.onNext(Empty.getDefaultInstance())
+                responseObserver.onCompleted()
+            }
+
+            else -> responseObserver.onError(Status.UNAUTHENTICATED.withDescription("Invalid credentials").asRuntimeException())
         }
     }
 }
